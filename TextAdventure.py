1b7891a862484441be9b762b32ada368ca670b56
Alex <alex.f.southworth@gmail.com>
2016-10-03 20:27:13 +0100

    added new function (i think)

--- TextAdventure.py
+++ TextAdventure.py
@@ -5,8 +5,9 @@ def main():
   from random import randint
   
   print ("Hi")
-  name = input ("What's your username?\n")
+  name = input ("What's your name?\n")
   print ("OK. Hello, " + name)
+  print ("A pen and paper may be necessary to write down given commands.")
   sleep (0.5)
   print ("Chapter one...")
   sleep (0.3)
@@ -14,8 +15,8 @@ def main():
   sleep (0.4)
   optionsloop = True
   while optionsloop == True:
-    ui1 = input ("Type 'options' to see the options.\n")
-    if ui1 == ("options"):
+    ui1 = input ("Type 'Options' to see the options.\n")
+    if ui1 == ("Options"):
       print ("Upstairs\nKitchen\nLounge\nOutside")
       optionsloop = False
     else: 
@@ -34,18 +35,52 @@ def main():
       print ("The front door is locked.")
     else:
       print ("That is not a valid answer")
-  inv = []
+  foodstock = []
   searchloop = True
   while searchloop == True:
     print ("You need to eat something. You will want to search this room.")
     sleep(0.1)
-    ui1 = input ("Type Search to search somewhere in this room.\n")
+    ui1 = input ("Type 'Search' to search somewhere in this room.\n")
     if ui1 == ("Search"):
       ui1 = input ("Search what? The options are: \ncoffee table\ndrawers\nbookshelf\nsofa\n")
       if ui1 == ("coffee table"):
-        print ("You found an apple! This will be added to your inventory. Type 'inv' at any time to view your inventory. ")
+        print ("You found an apple and some water! This will be added to your food stock. Type 'food' at any time to view your food stock. ")
+        foodstock.insert(0,"apple")
+        foodstock.insert(1,"water")
+        
+        string = foodstock[0]
+        for item in foodstock[1:]:
+          string = string + "\n" + item
+        print (string)
+        
+        searchloop = False
+      elif ui1 == ("sofa"):
+        print ("There is actually a million dollars inside this sofa. Shame they're useless. ")
+      elif ui1 == ("bookshelf"):
+        print ("Ugh, just a bunch of books. None of them are any use.")
+      elif ui1 == ("drawers"):
+        print ("Hmm... Wires, cables, a keyboard... nothing of any use to you.")
+      else:
+        print ("That is an invalid answer. ")
+    else:
+      print ("That is not a valid answer. ")
+  
+  consumeloop = True
+  while consumeloop == True:
+    print ("Now you have something to eat, so eat it.")
+    sleep(0.1)
+    ui1 = input ("Type 'Consume' to consume something in your food stock.\n")
+    if ui1 == ("Consume"):
+      ui1 = input ("Consume what? The options are: " + string)
+      if ui1 == ("apple"):
+        print ("Apple consumed. You now have 20/20 health. Type 'hp' to view your health at any time. ")
         inv.insert(0,"apple")
-        print (inv)
+        
+        string = inv[0]
+        for item in inv[1:]:
+          string = string + "\n" + item
+        print (string)
+        
         searchloop = False
       elif ui1 == ("sofa"):
         print ("There is actually a million dollars inside this sofa. Shame they're useless. ")
